c39c7fb852cee181a58155f7ab17d0e2b3a709b3
Markus Kaiser <markus.kaiser@siemens.com>
2021-05-25 13:33:00 +0100

    Store keets in Table; rebuild timelines on follow

--- kafker.py
+++ kafker.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+import itertools as it
+from typing import List, Set
 import faust
 import faust.cli
 
@@ -21,28 +23,37 @@ app = faust.App(
 )
 
 incoming_keets = app.topic("incoming_keets", value_type=Keet)
-keets = app.topic("keets", value_type=Keet)
+new_keets = app.topic("new_keets", value_type=Keet)
 follows = app.topic("follows", value_type=Follow)
+timeline_rebuilds = app.topic("timeline_rebuild", value_type=str)
 
+keets = app.Table("keets", default=list)
 timelines = app.Table("timelines", default=list)
+followings = app.SetTable("followings", default=set)
 followers = app.SetTable("followers", default=set)
 
 
-@app.agent(incoming_keets)
+@app.agent(incoming_keets, sink=[new_keets])
 async def process_incoming_keets(incoming_keets):
     async for keet in incoming_keets:
-        await keets.send(
-            value=Keet(
-                author=keet.author,
-                text=keet.text,
-                timestamp=int(datetime.now().timestamp()),
-            )
+        yield Keet(
+            author=keet.author,
+            text=keet.text,
+            timestamp=int(datetime.now().timestamp()),
         )
 
 
-@app.agent(keets)
-async def add_to_timeline(keets):
-    async for keet in keets.group_by(Keet.author):
+@app.agent(new_keets)
+async def persist_keet(new_keets):
+    async for keet in new_keets.group_by(Keet.author):
+        author_keets = keets[keet.author]
+        author_keets.append(keet)
+        keets[keet.author] = author_keets
+
+
+@app.agent(new_keets)
+async def append_timelines_with_new_keets(new_keets):
+    async for keet in new_keets.group_by(Keet.author):
         for author in {keet.author, *followers[keet.author]}:
             timeline = timelines[author]
             timeline.append(keet)
@@ -53,9 +64,22 @@ async def add_to_timeline(keets):
 async def process_follows(follows):
     async for follow in follows.group_by(Follow.passive_author):
         if follow.follow:
+            followings[follow.active_author].add(follow.passive_author)
             followers[follow.passive_author].add(follow.active_author)
         else:
+            followings[follow.active_author].discard(follow.passive_author)
             followers[follow.passive_author].discard(follow.active_author)
+
+        await timeline_rebuilds.send(value=follow.active_author)
+
+
+@app.agent(timeline_rebuilds)
+async def rebuild_timeline(timeline_rebuilds):
+    async for author in timeline_rebuilds.group_by(lambda x: x, name="by-author"):
+        timelines[author] = sorted(
+            it.chain(*(keets[athr] for athr in {author, *followings[author]})),
+            key=lambda keet: keet.timestamp,
+        )
 
 
 @app.page("/timeline/{author}/")
