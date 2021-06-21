406c2bd775bacca603e069d3f83ef92c068e2bc7
Markus Kaiser <markus.kaiser@siemens.com>
2021-06-21 15:00:29 +0100

    Add personal dictionaries

--- kafker/markov/agents.py
+++ kafker/markov/agents.py
@@ -1,11 +1,19 @@
 from kafker.app import app
-from kafker.markov.models import bigram_weights, bigrams, new_bigrams, new_texts
-from kafker.posts.models import Post, new_posts
+from kafker.markov.models import (
+    bigram_weights,
+    bigrams,
+    new_bigrams,
+    new_texts,
+    personal_dictionary,
+    personal_dictionary_rebuilds,
+)
+from kafker.posts.models import Post, new_posts, posts, posts_by_author
 
 
 @app.agent(new_posts, sink=[new_texts])
 async def submit_text(new_posts):
     async for post in new_posts.group_by(Post.author):
+        await personal_dictionary_rebuilds.send(value=post.author)
         yield post.text
 
 
@@ -23,3 +31,19 @@ async def persist_bigrams(new_bigrams):
     async for lhs, rhs in new_bigrams:
         bigrams[lhs].add(rhs)
         bigram_weights[lhs, rhs] += 1
+
+
+@app.agent(personal_dictionary_rebuilds)
+async def rebuild_personal_weights(personal_dictionary_rebuilds):
+    async for author in personal_dictionary_rebuilds.group_by(
+        lambda x: x, name="by-author"
+    ):
+        print(f"Rebuilding {author}")
+        for post_id in posts_by_author[author]:
+            post = posts.get(post_id.uid, None)
+            print(f"{post_id=} with {post=}")
+            if post is not None:
+                text = post.text
+                words = text.split(" ")
+                for word in words:
+                    personal_dictionary[author].add(word)
